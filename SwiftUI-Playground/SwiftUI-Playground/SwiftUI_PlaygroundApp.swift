511ad0566178dfdb094d9ac0cbdb6f4b25e47536
Dai Kawahara <skbsporta7@gmail.com>
2022-06-24 17:46:32 +0900

    Initial commit

---
+++ SwiftUI-Playground/SwiftUI-Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI-Playground
+//
+//  Created by kawaharadai on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
