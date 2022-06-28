493bb49afba5259591e3496389276b1d9810fb34
Dai Kawahara <skbsporta7@gmail.com>
2022-06-29 08:14:41 +0900

    add stack views

--- SwiftUI-Playground/SwiftUI-Playground/StackView.swift
+++ SwiftUI-Playground/SwiftUI-Playground/StackView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct StackView: View {
+    let colors: [Color] =
+    [.red, .orange, .yellow, .green, .blue, .purple]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // 縦並べ
+        VStack(
+            alignment: .leading,
+            spacing: 0
+        ) {
+            // 横並べ
+            HStack(spacing: 10) {
+                ForEach(
+                    0...5,
+                    id: \.self
+                ) {
+                    Text("Item \($0)")
+                }
+            }
+            // 横並べをスクロールで表示されるまで生成しない形で
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 10) {
+                    ForEach(1...100, id: \.self) {
+                        Text("Column \($0)")
+                    }
+                }
+            }
+            .frame(height: 50)
+            .scrollIndicators(.hidden)
+
+            // 縦並べをスクロールで表示されるまで生成しない形でScrollViewは基本.verticalらしい
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(1...100, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                }
+            }.frame(height: 300)
+            // Z軸で10ずつズラして100四方の正方形を配置
+            ZStack {
+                ForEach(
+                    0..<colors.count,
+                    id: \.self
+                ) {
+                    Rectangle()
+                        .fill(colors[$0])
+                        .frame(width: 100, height: 100)
+                        .offset(x: CGFloat($0) * 10.0,
+                                y: CGFloat($0) * 10.0)
+                }
+            }
+            Spacer()
+        }
     }
 }
 
