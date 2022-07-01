1b414f7035b52c665201a65abdb5c8fb0a198c3a
Dai Kawahara <skbsporta7@gmail.com>
2022-07-01 09:31:03 +0900

    add grid

--- SwiftUI-Playground/SwiftUI-Playground/GridView.swift
+++ SwiftUI-Playground/SwiftUI-Playground/GridView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct GridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // グリッド表示（スクロールが必要なものはVGridなどで代用する）
+            Grid(alignment: .center,
+                 horizontalSpacing: 1,
+                 verticalSpacing: 1) {
+                GridRow {
+                    Text("GridRow 1")
+                    ForEach(0..<2) { _ in Color.red }
+                }.frame(width: 50, height: 50)
+                GridRow {
+                    Text("GridRow 2")
+                    ForEach(0..<6) { _ in Color.green }
+                }.frame(width: 50, height: 50)
+                GridRow {
+                    Text("GridRow 3")
+                    ForEach(0..<4) { _ in Color.blue }
+                }.frame(width: 50, height: 50)
+            }
+            Spacer()
+        }
     }
 }
 
