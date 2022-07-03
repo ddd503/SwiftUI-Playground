df515270d476ea79bc23d0349eb2c303faa85f02
Dai Kawahara <skbsporta7@gmail.com>
2022-07-03 09:35:14 +0900

    add grid anchor

--- SwiftUI-Playground/SwiftUI-Playground/GridView.swift
+++ SwiftUI-Playground/SwiftUI-Playground/GridView.swift
@@ -11,21 +11,29 @@ struct GridView: View {
     var body: some View {
         VStack {
             // グリッド表示（スクロールが必要なものはVGridなどで代用する）
-            Grid(alignment: .center,
-                 horizontalSpacing: 1,
+            Grid(horizontalSpacing: 1,
                  verticalSpacing: 1) {
                 GridRow {
                     Text("GridRow 1")
                     ForEach(0..<2) { _ in Color.red }
-                }.frame(width: 50, height: 50)
+                        .frame(width: 50, height: 50)
+                }
                 GridRow {
                     Text("GridRow 2")
                     ForEach(0..<6) { _ in Color.green }
-                }.frame(width: 50, height: 50)
+                        .frame(width: 50, height: 50)
+                }
                 GridRow {
                     Text("GridRow 3")
-                    ForEach(0..<4) { _ in Color.blue }
-                }.frame(width: 50, height: 50)
+                    Color.red.frame(width: 50, height: 50)
+                    // 一部セルの座標を変える
+                    Color.blue.frame(width: 10, height: 10)
+                        .gridCellAnchor(.topTrailing)
+                    Color.green.frame(width: 20, height: 20)
+                        .gridCellAnchor(UnitPoint(x: 0.8, y: 0.9))
+                    Color.yellow.frame(width: 20, height: 20)
+                        .gridCellColumns(2) // セル個数分配置方向にずらす（2つ分ずらす）
+                }
             }
             Spacer()
         }
